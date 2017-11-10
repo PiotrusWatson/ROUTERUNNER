79ddcc54fcb562c13a8e53a72f0d40a48cdf3a3b
PiotrusWatson <2200577w@bo720-9-02u.dcs.gla.ac.uk>
2017-11-10 15:47:38 +0000

    added broke ass script

--- scripts/uploadfile.js
+++ scripts/uploadfile.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
+
+  $('#drop').on({'dragover dragenter': function(e){
+	e.preventDefault();
+	e.stopPropagation();
+  },
+  'drop': function(e) {
+	var data = e.originalEvent.dataTransfer;
+
+	if (data != null && data.files.length){
+  		e.preventDefault();
+  		e.stopPropagation();
+		$.each(data.files, function(i, file){
+		  	console.log("FILE READ" + i);
+			var reader = new FileReader();
+			reader.onload = $.proxy(function (file, $list, event){	
+			$list.prepend($("<li>").append(file.name));
+			}, this, file, $('#list'));
+		  	//reader.readAsDataURL(file);	
+		}		  
+	}
+
+  });
   
 });
