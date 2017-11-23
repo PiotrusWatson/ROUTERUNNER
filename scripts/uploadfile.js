58045e64705495baa8c68c851323e76e37661ab6
PiotrusWatson <2200577w@bo1028-1-06u.dcs.gla.ac.uk>
2017-11-23 10:41:08 +0000

    now read files completely

--- scripts/uploadfile.js
+++ scripts/uploadfile.js
@@ -1,13 +1,25 @@
 var files = []; 
+
 function doTheDrag(e){
 	e.stopPropagation();
     e.preventDefault();
     e.dataTransfer.dropEffect = 'copy';
 
 }
+function readFile(f){
+	var reader = new FileReader();
+	reader.onload = (function(reader)
+	 { return function(){
+						  contents = reader.result;
+						  var lines = contents.split('\n');
+						  console.log(contents);
+						}
+	 })(reader);
+	reader.readAsText(f);
+}
 
 function doTheDrop(e){
-  	//stop normal things from happening
+  	//stop normal things from	console.log(files); happening
  	e.stopPropagation();
     e.preventDefault();
 	
@@ -23,24 +35,29 @@ function doTheClick(e){
 }
 
 function doTheUpload(uploadedfile){ //checks if file is The Correct Mime Type
-  if (uploadedfile.type() == 'text/xml'){
-	files.push(uploadedfile);
-	return true;
-  }
-  else {
-	console.log("FUCK");
-	return false;
-  }	
+  //currently broken until find correct mimetype/other solution 
+  readFile(uploadedfile);
+ }
+
+function parseGPX(text){
+	parser = new DOMParser();
+	window.xml = parser.parseFromString(text, "text/xml");
+	console.log(window.xml);
+
 }
+
+
+
 
 $(document).ready(function(){
 
-
+	//handling all different types of interaction with that middle block - clicks + drags!!
 	var dropZone = document.getElementById('drop');
 	var clickBox = $('#secretclickbox');
 	dropZone.addEventListener('dragover', doTheDrag, false);
 	dropZone.addEventListener('drop', doTheDrop, false);
 	dropZone.addEventListener('click',doTheClick, false); 
+
 	clickBox.on('change', function(e){
 	  doTheUpload(e.target.files[0]);
 	  console.log(files);
